d94f43b592591cc0ee3c57d5364d4a68cfe8f187
legacyKim <123148778+legacyKim@users.noreply.github.com>
2023-08-31 09:01:47 +0900

    data lose

---
+++ src/data.js
@@ -0,0 +1,28 @@
+let WriteData = [
+
+    {
+        id: 0,
+        title: "test 01",
+        subTitle : "test subtitle 01",
+        content : "content 01"
+    },
+
+    {
+        id: 1,
+        memo: "test 02",
+    },
+
+    {
+        id: 2,
+        memo: "test 03",
+    },
+
+    {
+        id: 3,
+        memo: "test 04",
+    },
+
+
+]
+
+export default WriteData;
