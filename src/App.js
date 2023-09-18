ec9ee2a0a88b6b3a2f4e9ece9170fcba29553bdb
legacyKim <123148778+legacyKim@users.noreply.github.com>
2023-09-19 00:19:12 +0900

    header scroll event

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ import Routes from './Routes'
 function App() {
 
     const navigate = useNavigate();
-
     const [theme, themeChange] = useState('dark');
 
     const themeChangeBtn = () => {
@@ -24,17 +23,47 @@ function App() {
     }
 
     const [isSearchOn, setIsSearchOn] = useState(false);
-    const searchOn = (e) => {
-        setIsSearchOn(!isSearchOn);
-        // e.stopPropagaion();
-    }
+    const [searchActive, setSearchActive] = useState('');
 
-    // const Page: FC<{ to: string }> = (props) => <main><Link {...props} /></main>; // prettier-ignore
+    const searchOn = () => {
+        setIsSearchOn(!isSearchOn);
+        if (!isSearchOn) {
+            setSearchActive('active');
+        } else {
+            setSearchActive('');
+        }
+    };
+
+    const closeSearch = () => {
+        setTimeout(() => {
+            setIsSearchOn(false);
+        }, 300);
+
+        setSearchActive('');
+    };
+
+    useEffect(() => {
+        if (searchActive) {
+            setSearchActive('active');
+        } else {
+            setSearchActive('');
+        }
+    }, [searchActive]);
+
+    const [scrollPosition, setScrollPosition] = useState(0);
+
+    const updateScroll = () => {
+        setScrollPosition(window.scrollY || document.documentElement.scrollTop);
+    };
+    
+    useEffect(() => {
+        window.addEventListener('scroll', updateScroll);
+    }, []);
 
     return (
 
-        <div className={`App ${theme}`}>
-            <div className='header'>
+        <div id='app' className={`App ${theme}`}>
+            <div className={`header ${scrollPosition > 0 ? "active" : ""}`}>
 
                 <div className='logo'>
                     <NavLink to="/" className='icon-github-circled-alt2' onClick={() => { navigate('/') }}></NavLink>
@@ -44,7 +73,7 @@ function App() {
                     <li className='btn'><NavLink to="/components/Write" className='icon-vector-pencil' onClick={() => { navigate('/components/Write') }}></NavLink></li>
                     <li className='btn'><NavLink to="/components/WriteList" className='icon-clipboard' onClick={() => { navigate('/components/WriteList') }}></NavLink></li>
 
-                    <li className='btn'><NavLink to="/components/Memo" className='' onClick={() => { navigate('/components/Memo') }}></NavLink></li>
+                    <li className='btn'><NavLink to="/components/Memo" className='icon-comment' onClick={() => { navigate('/components/Memo') }}></NavLink></li>
                     <li className='btn'><button className='icon-search' onClick={searchOn}></button></li>
 
                     {/* <li className='index_btn'>
@@ -57,46 +86,24 @@ function App() {
             </div>
 
             <Routes></Routes>
-            <Search></Search>
+            {/* <Search isSearchOn={isSearchOn} searchActive={searchActive}></Search> */}
 
+            <div className={`search ${searchActive ? searchActive : ""}`}>
+                <div className="search_box">
+                    <button className='w_color icon-cancel' onClick={closeSearch}></button>
+                    <div className="search_input">
+                        <input type='text'></input>
+                    </div>
+                    <ol className='search_list'>
+                        <li>
+                        </li>
+
+                    </ol>
+                </div>
+            </div>
         </div>
 
     )
-
-    function Search() {
-
-        let [searchActive, setSearchActive] = useState('')
-
-        const closeSearch = () => {
-            setTimeout(() => {
-              setIsSearchOn(false);
-            }, 300);
-        
-            setSearchActive('');
-          };
-
-        useEffect(() => {
-            if (isSearchOn) {
-                setSearchActive('active');
-              } else {
-                setSearchActive('');
-              }
-        }, [isSearchOn]);
-
-        return (
-
-            <div className={`search ${isSearchOn != false ? searchActive : ""}`}>
-                <div className="search_box">
-                    <input type='text'></input>
-                    <button className='w_color' onClick={closeSearch}>
-                        임시닫기
-                    </button>
-                </div>
-            </div>
-
-        )
-
-    }
 
 }
 
