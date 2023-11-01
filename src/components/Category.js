55c1092489f2ff5c3c3d6144851bb785bb55339d
legacyKim <123148778+legacyKim@users.noreply.github.com>
2023-11-01 21:07:47 +0900

    Category

--- src/components/Category.js
+++ src/components/Category.js
@@ -9,28 +9,58 @@ function Category() {
 
     let cateListData = useSelector((state) => state.cateData);
 
+    // catelist scroll event
+    const [cateScroll, cateScrollPos] = useState(0);
+    var cateScrollArea = useRef();
+
+    const cateScrollMove = () => {
+        cateScrollPos(cateScrollArea.current.scrollTop);
+        console.log(cateScrollArea);
+        console.log(cateScrollArea.current);
+        console.log(cateScrollArea.current.scrollTop);
+    };
+
+    useEffect(() => {
+        cateScrollArea.addEventListener('scroll', cateScrollMove);
+        console.log(cateScroll);
+    }, [cateScroll]);
+    // catelist scroll event
+
     return (
 
         <div className='content_area'>
-
-            {
-                cateListData.map(function (a, i) {
-                    return (
-                        <div key={i}>
-                            <CategoryList cate={a}></CategoryList>
-                        </div>
-                    )
-                })
-            }
-
+            <div className='cate_list_pos'>
+                <ul className='cate_list' ref={cateScrollArea}>
+                    {
+                        cateListData.map(function (a, i) {
+                            return (
+                                <li key={i}>
+                                    <CategoryList cate={a}></CategoryList>
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
+            </div>
         </div>
 
     )
 
-    function CategoryList({cate}) {
+    function CategoryList({ cate }) {
+
+        const [cateActive, cateActiveStyle] = useState(false);
+        const cateClick = () => {
+            cateActiveStyle(!cateActive);
+            if (!cateActive) {
+                cateActiveStyle('active');
+            } else {
+                cateActiveStyle('');
+            }
+
+        };
 
         return (
-            <div>{cate}</div>
+            <span className={`${cateActive ? "active" : ""}`} onClick={cateClick}>{cate}</span>
         )
     }
 
