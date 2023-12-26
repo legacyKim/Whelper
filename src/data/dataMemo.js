79e7658cb4118999d790e2bbedc9a8f7797cde35
legacyKim <arbam486@gmail.com>
2023-12-26 19:02:57 +0900

    메모

--- src/data/dataMemo.js
+++ src/data/dataMemo.js
@@ -3,7 +3,6 @@ let memoListData = [
     {
         id: 0,
         memoComment: "신은 죽었다. 아무것도 허용되지 않는다.",
-        memoOwner : "자크라캉",
         memoSource: "에크리",
         memoKeyword : "반니체주의",
     },
@@ -11,7 +10,6 @@ let memoListData = [
     {
         id: 1,
         memoComment: "인간은 실패할지언정 파괴되진 않는다.",
-        memoOwner : "어니스트 헤밍웨이",
         memoSource: "노인과 바다",
         memoKeyword : "실존주의",
     },
@@ -19,7 +17,6 @@ let memoListData = [
     {
         id: 2,
         memoComment: "생각한 일은 가능한 일이기도 하다. 불가능은 논리적 불가능이다.",
-        memoOwner : "루트비히 비트겐슈타인",
         memoSource: "논리-철학논고",
         memoKeyword : "사태, 사건 그리고 표상 속의 주체",
     },
@@ -27,8 +24,7 @@ let memoListData = [
     {
         id: 3,
         memoComment: "의지가 정말로 그 문제를 해결할 수 있다고 믿으려면, 그 문제를 조건화할 수 있을 때에만 그렇다.",
-        memoOwner : "칼 구스타프 융, ",
-        memoSource: "심리유형>",
+        memoSource: "심리유형",
         memoKeyword : "의지와 인식에 대한 이해",
     },
 
